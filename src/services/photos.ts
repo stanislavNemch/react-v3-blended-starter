08e19bae9cfd679eb16fac02b15827220611bc38
Stanislav Nemchenko <stas.buka@gmail.com>
2025-08-29 01:18:37 +0300

    first commit

--- src/services/photos.ts
+++ src/services/photos.ts
@@ -1,14 +1,14 @@
 import axios from "axios";
 
-const API_KEY = "563492ad6f9170000100000108dc2880626e4436b3634ce1cf6b4d74";
+const API_KEY = import.meta.env.VITE_API_KEY_PEXELS;
 axios.defaults.baseURL = "https://api.pexels.com/v1/";
 axios.defaults.headers.common["Authorization"] = API_KEY;
 axios.defaults.params = {
-  orientation: "landscape",
+    orientation: "landscape",
 };
 
-export const getPhotos = async (query) => {
-  const response = await axios.get(`search?query=${query}`);
+export const getPhotos = async (query: string) => {
+    const response = await axios.get(`search?query=${query}`);
 
-  return response.data.photos;
+    return response.data.photos;
 };
